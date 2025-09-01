3364385c5ff68a7487939be601833e730793f63a
BOO JIA JUN <b032310805@student.utem.edu.my>
2025-09-01 23:16:36 +0800

    debug 34

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
